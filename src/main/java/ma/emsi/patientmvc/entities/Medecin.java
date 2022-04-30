e75bde3f7864fccae5ff78f12180f6bccc90d2b5
Nizar Sbai <nizar1.sbai@gmail.com>
2022-04-30 14:23:13 +0000

    L'Ajout du classe Medecin

---
+++ src/main/java/ma/emsi/patientmvc/entities/Medecin.java
@@ -0,0 +1,26 @@
+package ma.emsi.patientmvc.entities;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+import java.util.Date;
+
+public class Medecin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotEmpty
+    @Size(min = 3, max=40)
+    private String nom;
+    @NotEmpty
+    @Size(min = 3, max=40)
+    private String prenom;
+    @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date dateNaissance;
+    @NotEmpty
+    @Size(min = 3, max=10)
+    private String CIN;
+}
