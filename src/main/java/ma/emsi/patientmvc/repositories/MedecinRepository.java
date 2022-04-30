2753579a3dc4078c044dc9e0fad2cc14eb6bc6f8
Nizar Sbai <nizar1.sbai@gmail.com>
2022-04-30 16:35:03 +0000

    Modification sur le compte rendu

--- src/main/java/ma/emsi/patientmvc/repositories/MedecinRepository.java
+++ src/main/java/ma/emsi/patientmvc/repositories/MedecinRepository.java
@@ -3,5 +3,5 @@ package ma.emsi.patientmvc.repositories;
 import ma.emsi.patientmvc.entities.Medecin;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MedecinRepository extends JpaRepository<Medecin, Long> {
+public interface MedecinRepository  {
 }
