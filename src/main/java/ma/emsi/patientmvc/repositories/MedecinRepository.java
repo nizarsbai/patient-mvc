a8b68cbafbd72011ac18579cc2b0bf3ac2615c1e
Nizar Sbai <nizar1.sbai@gmail.com>
2022-04-30 14:26:43 +0000

    L'Ajout du classe Medecin avec son Interface

---
+++ src/main/java/ma/emsi/patientmvc/repositories/MedecinRepository.java
@@ -0,0 +1,7 @@
+package ma.emsi.patientmvc.repositories;
+
+import ma.emsi.patientmvc.entities.Medecin;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MedecinRepository extends JpaRepository<Medecin, Long> {
+}
